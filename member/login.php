d8ae215c813e88c1a94b136a70207cc2d4cdc487
jmn04 <jmn2012km@gmail.com>
2023-12-15 17:10:11 +0900

    ログインページhtml構造作成

---
+++ member/login.php
@@ -0,0 +1,26 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+</head>
+<body>
+    <h1>ログイン</h1>
+    <div id="form">
+        <div id="input_wrapper" class="form_contents">
+            <h3>会員の方</h3>
+            <p>メールアドレスとパスワードでログイン</p>
+            <form action="post">
+                <div id="input_area">
+                    <input type="email" placeholder="メールアドレス">
+                    <input type="password" placeholder="パスワード">
+                    <button type="submit">ログイン</button>
+                </div>
+            </form>
+        </div>
+
+        <div id="transition_wrapper" class="form_contents">
+            <h3>新規会員登録</h3>
+            <a href="#">新規会員登録</a>
+        </div>
+    </div>
+</body>
