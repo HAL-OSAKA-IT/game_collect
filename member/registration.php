15d738c951f22a07b2ba06cd626c0eb85fe038d0
Okinopi <114094511+okinopita@users.noreply.github.com>
2023-12-15 18:02:34 +0900

    snippetsの共有

--- member/registration.php
+++ member/registration.php
@@ -1,1 +1,18 @@
-test02
+<?php 
+// あとで消すsnippetsの共有
+?>
+// PHP tag snippets
+"php tag": {
+    "prefix": "php",
+    "body": [
+        "<?php $1 ?>"
+    ],
+    "description": "PHP tag"
+},
+"php echo": {
+    "prefix": "phpe",
+    "body": [
+        "<?= $1 ?>"
+    ],
+    "description": "PHP echo tag"
+}
