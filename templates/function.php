d5155c9677b822c5131943c5455c783b38df63a3
Toshiki <wata.hal.student@gmail.com>
2023-12-16 07:30:55 +0900

    作成 環境変数関係
    環境変数に必要なファイルのコミット

--- templates/function.php
+++ templates/function.php
@@ -1,11 +1,14 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
 // データベースの接続関数
 function db_connect(){
     try{
         // データソースネーム
-        $dsn = 'mysql:dbname=game_collect;host=localhost;charset=utf8mb4';    
-        $user = 'root';
-        $password = '';
+        $dsn = 'mysql:dbname='.$_ENV['DB_NAME'].';host='.$_ENV['DB_HOST'].';charset=utf8mb4';    
+        $user = $_ENV['DB_USER'];
+        $password = $_ENV['DB_PASS'];
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
