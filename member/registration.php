bd0b8d02311cf71564e7bf20856d20c5f1f78c9a
Okinopi <114094511+okinopita@users.noreply.github.com>
2023-12-15 18:42:55 +0900

    一時的な変更

--- member/registration.php
+++ member/registration.php
@@ -1,22 +1,36 @@
 <?php 
-// あとで消すsnippetsの共有
-?>
-// PHP tag snippets
-"php tag": {
-    "prefix": "php",
-    "body": [
-        "<?php $1 ?>"
-    ],
-    "description": "PHP tag"
-},
-"php echo": {
-    "prefix": "phpe",
-    "body": [
-        "<?= $1 ?>"
-    ],
-    "description": "PHP echo tag"
-} 
+// やること
+// 1. scssファイル作成パスを相対パスに変える(木下の家のwindowsに設定が残ってるはずなのでそれを反映する。)
 
-<!-- やること -->
-<!-- 1. scssファイル作成パスを相対パスに変える(木下の家のwindowsに設定が残ってるはずなのでそれを反映する。) -->
-<!--  -->
+
+echo 'hello world';
+$variable = 12;
+// 文字列連結はピリオド
+$str = 'aaaaaa' . 'aaaaaa';
+
+$array = ['にんじん', 'しりしり'];
+// 配列などの文字列以外の値はechoするとエラーが出る
+print_r($array);
+
+?>
+
+
+<h1>変数は<?php echo $variable; ?></h1>
+
+<!-- phpタグとecho文を兼ね備えたタグ -->
+<p>
+<?= 'php echo aaaaaaaaaaaaaaaaaaaaaa';?>
+</p>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
