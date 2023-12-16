87d5595a2fba7765768d09dc1db99bb3debe1a3c
gomadango0141 <hayato31halit@gmail.com>
2023-12-16 17:11:59 +0900

    テスト　コミット

--- member/registration.php
+++ member/registration.php
@@ -28,7 +28,7 @@ print_r($array);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>会員登録</title>
-    <link rel="stylesheet" href="../css/destyle.css">
+    <!-- <link rel="stylesheet" href="../css/destyle.css"> -->
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
