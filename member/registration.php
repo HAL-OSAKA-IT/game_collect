b8e93c977ed80519e2c1f853563b0e90aa2d1962
gomadango0141 <hayato31halit@gmail.com>
2023-12-16 18:46:44 +0900

    scss 仮

--- member/registration.php
+++ member/registration.php
@@ -1,6 +1,6 @@
 <?php 
-include '../templates/function.php';   // 関数を記述したファイルの読み込み
-$dbh = db_connect();                // データベース接続
+// include '../templates/function.php';   // 関数を記述したファイルの読み込み
+// $dbh = db_connect();                // データベース接続
 // 返り値がある場合(SELECT)
 // $sql = "
 //         SELECT *
@@ -10,37 +10,37 @@ $dbh = db_connect();                // データベース接続
 // $stmt -> execute();
 // $array = $stmt -> fetchAll();
 // print_r($array);
-echo '<br>';
+// echo '<br>';
 
 // 返り値の無い変更の場合(INSERTとか)
-$id = 'NULL';
-$name = 'namae';
-$sql = "
-    INSERT INTO games(id, name)
-    VALUE($id, '$name')
-";
-$stmt = $dbh -> prepare($sql);
-$stmt -> execute();
+// $id = 'NULL';
+// $name = 'namae';
+// $sql = "
+//     INSERT INTO games(id, name)
+//     VALUE($id, '$name')
+// ";
+// $stmt = $dbh -> prepare($sql);
+// $stmt -> execute();
 
 
-echo 'hello world';
-$variable = 12;
-// 文字列連結はピリオド
-$str = 'aaaaaa' . 'aaaaaa';
+// echo 'hello world';
+// $variable = 12;
+// // 文字列連結はピリオド
+// $str = 'aaaaaa' . 'aaaaaa';
 
-$yasai = ['にんじん', 'しりしり'];
-// 配列などの文字列以外の値はechoするとエラーが出る
-print_r($yasai);
+// $yasai = ['にんじん', 'しりしり'];
+// // 配列などの文字列以外の値はechoするとエラーが出る
+// print_r($yasai);
 
 ?>
 
 
-<h1>変数は<?php echo $variable; ?></h1>
+<!-- <h1>変数は<?php echo $variable; ?></h1> -->
 
 <!-- phpタグとecho文を兼ね備えたタグ -->
-<p>
+<!-- <p>
 <?= 'php echo aaaaaaaaaaaaaaaaaaaaaa';?>
-</p>
+</p> -->
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -48,18 +48,18 @@ print_r($yasai);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>会員登録</title>
-    <!-- <link rel="stylesheet" href="../css/destyle.css"> -->
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="../css/destyle.css">
+    <link rel="stylesheet" href="./css/form.css">
 </head>
 <body>
     <!-- <h1>会員登録</h1> -->
     <div id="form">
         <div id="transition_wrapper" class="form_contents">
-            <h3>新規会員登録</h3>
-            <p>メールアドレスとパスワードで会員登録</p>
+            <h2>新規会員登録</h2>
+            <p>ニックネームとパスワードで会員登録</p>
             <form action="post">
                 <div id="input_area">
-                    <input type="email" placeholder="メールアドレス">
+                    <input type="text" placeholder="ニックネーム">
                     <input type="password" placeholder="パスワード">
                     <button type="submit">新規会員登録</button>
                 </div>
