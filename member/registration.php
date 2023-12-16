7e1f89e19c08c27b4ea5a8872fb39d797cc9196c
gomadango0141 <hayato31halit@gmail.com>
2023-12-16 13:16:11 +0900

    php変更

--- member/registration.php
+++ member/registration.php
@@ -23,14 +23,23 @@ print_r($array);
 </p>
 
 
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>会員登録</title>
 </head>
 <body>
-    
+    <h1>会員登録</h1>
+    <div id="form">
+        <div id="transition_wrapper" class="form_contents">
+            <h3>新規会員登録</h3>
+            <p>メールアドレスとパスワードで会員登録</p>
+            <form action="post">
+                <div id="input_area">
+                    <input type="email" placeholder="メールアドレス">
+                    <input type="password" placeholder="パスワード">
+                    <button type="submit">新規会員登録</button>
+                </div>
+            </form>
+        </div>
 </body>
-</html>
