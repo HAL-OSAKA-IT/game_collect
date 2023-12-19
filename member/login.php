3762f08d2f1f21f05f33718a1f2eab94def31998
jmn04 <jmn2012km@gmail.com>
2023-12-19 14:48:22 +0900

    ログインページ input修正

--- member/login.php
+++ member/login.php
@@ -2,24 +2,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン</title>
+    <!-- リセットCSS設定 -->
+    <link rel="stylesheet" href="../css/destyle.css">
+    <!-- 登録・ログイン共通CSS設定 -->
+    <link rel="stylesheet" href="./css/form.css">
 </head>
 <body>
     <h1>ログイン</h1>
     <div id="form">
         <div id="input_wrapper" class="form_contents">
-            <h3>会員の方</h3>
-            <p>メールアドレスとパスワードでログイン</p>
+            <h2>会員の方</h2>
+            <p>ユーザー名とパスワードでログイン</p>
             <form action="post">
                 <div id="input_area">
-                    <input type="email" placeholder="メールアドレス">
-                    <input type="password" placeholder="パスワード">
+                    <input type="text" placeholder="ユーザー名" name="name">
+                    <input type="password" placeholder="パスワード" name="password">
                     <button type="submit">ログイン</button>
                 </div>
             </form>
         </div>
 
         <div id="transition_wrapper" class="form_contents">
-            <h3>新規会員登録</h3>
+            <h2>新規会員登録</h2>
             <a href="#">新規会員登録</a>
         </div>
     </div>
