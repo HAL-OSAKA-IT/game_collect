ee3249251997f6c8b3668124eb64f431dbfa9fe3
F_momo <ohs20496@ohs.hal.ac.jp>
2023-12-20 21:08:26 +0900

    headerの作成

--- index.php
+++ index.php
@@ -6,8 +6,20 @@
     <title>Document</title>
     <!-- リセットCSSの適用 -->
     <link rel="stylesheet" href="./css/destyle.css">
+    <link rel="stylesheet" href="./css/style.css?=231220">
 </head>
 <body>
-
+<header>
+    <nav id="nav">
+        <h1>ロゴ</h1>
+        <ul id="menu_group">
+            <li id="menu_item">コンテンツ</li>
+            <li id="menu_item">コンテンツ</li>
+            <li id="menu_item">コンテンツ</li>
+            <li id="menu_item">コンテンツ</li>
+        </ul>
+        <p>ログイン/会員登録</p>
+    </nav>
+</header>
 </body>
 </html>
