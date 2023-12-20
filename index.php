b2864b2e50d39a4f27284ffd27e488f2ba00a47c
F_momo <ohs20496@ohs.hal.ac.jp>
2023-12-20 16:58:43 +0900

    scssデザイン 未完成

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- リセットCSSの適用 -->
     <link rel="stylesheet" href="./css/destyle.css">
+    <link rel="stylesheet" href="./css/style.css?=231220">
 </head>
+
 <body>
-ho
+    <div id="item">
+        <h2>ゲーム一覧</h2>
+        <div id="element">
+            <img src="./image/海月ご飯.jpeg" alt="">
+            <div id="detail">
+                <h3>タイトル</h3>
+                <p id="game">ゲーム紹介文</p>
+                <p id="info">情報</p>
+                <a href="#">ランキングを見る</a>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
