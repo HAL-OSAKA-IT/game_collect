362ad76ab2442d7a92de9c06b3749ee317eb1970
Yamato Kadowaki <118909709+jmn04@users.noreply.github.com>
2023-12-21 06:26:40 +0900

    変更レビューの反映

--- member/login.php
+++ member/login.php
@@ -1,44 +1,7 @@
 <?php
-// セッションチェック用スクリプト
-// require 'session-check.php';
 // game_collectがカレントディレクトリとなるpathを設定
 $path = '../';
 include $path.'templates/function.php';
-
-/* ================================================================================
-                        セッションチェック処理 ここから
-================================================================================ */
-// session-check.phpなどに記述して、表示用のページ全てにrequireするのが望ましい。
-session_start(); // セッションの開始 session-check.phpに記載すること
-$session_time_debug = false; // セッションタイムアウトのデバッグ用
-
-if ($session_time_debug){
-    // ログイン済みかどうかを、$_SESSION['name']の有無で判断
-    if (isset($_SESSION['name'])){
-        # ログイン済みの場合はindex.phpにリダイレクト
-        header('Location:' . $path);
-        exit();
-    }
-
-    // タイムアウト時間の設定
-    $timeout = 60 * 20; // 20分
-
-    // LAST_ACTIVITYが設定されていて、それがタイムアウトしている場合
-    if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > $timeout)) {
-        session_unset(); // セッション変数を全て削除
-        session_destroy(); // セッションを完全に破棄
-        # session-timeout.phpにリダイレクト
-        header("Location: ../session-timeout.php");
-        exit;
-    }
-
-    // 最後の行動時間を更新（現在時刻を代入）
-    $_SESSION['LAST_ACTIVITY'] = time();
-}
-/* ================================================================================
-                        セッションチェック用の処理 ここまで
-================================================================================ */
-
 
 
 /* ================================================================================
@@ -58,10 +21,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
         $stmt -> bindValue(':name', $name, PDO::PARAM_STR);
         $stmt -> execute();
 
-        $result = $stmt -> fetch(PDO::FETCH_ASSOC);
+        $result = $stmt -> fetch();
 
         if ($result && password_verify($password, $result['password'])){
-            $_SESSION['name'] = $result['name'];
+            $_SESSION['member_id'] = $result['id'];
             $_SESSION['LAST_ACTIVITY'] = time();
             // index.phpにリダイレクト
             header('Location:' . $path);
@@ -88,10 +51,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     <link rel="stylesheet" href="./css/form.css">
 </head>
 <body>
-    <h1>ログイン</h1>
+    <h2>ログイン</h2>
     <div id="form">
         <div id="input_wrapper" class="form_contents">
-            <h2>会員の方</h2>
+            <h3>会員の方</h3>
             <p>ユーザー名とパスワードでログイン</p>
             <!-- ユーザー名かパスワードが未入力の場合 -->
             <?php if(!empty($error['blank']) && $error['blank'] === 'blank'): ?>
@@ -110,7 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
         </div>
 
         <div id="transition_wrapper" class="form_contents">
-            <h2>新規会員登録</h2>
+            <h3>新規会員登録</h3>
             <a href="./registration.php">新規会員登録</a>
         </div>
     </div>
