ae11edf80ec9e82c281046c28e7b10b66c823336
Toshiki <wata.hal.student@gmail.com>
2023-12-24 16:10:18 +0900

    変更 トップページ
    CSSリンクのコメント追加
    include文の変更
    ゲーム表示部分をループに変更

--- index.php
+++ index.php
@@ -7,37 +7,27 @@
     <title>Document</title>
     <!-- リセットCSSの適用 -->
     <link rel="stylesheet" href="./css/destyle.css">
-    <link rel="stylesheet" href="./css/style.css?=231220">
+    <link rel="stylesheet" href="./css/style.css?=231220"> <!-- cssに変更を加えた時にクエリ文字を変更することでキャッシュが残らないようになります -->
 </head>
 
-<?php include ( dirname(__FILE__) . '/templates/header.php' ); ?>
+<?php include '/templates/header.php'; ?>
 <body>
     <main>
         <section id="game-list">
             <h2>ゲーム一覧</h2>
-            <div class="game">
-                <p class="game-image"><img src="./image/gameImage.png" alt="ゲーム名"></p>
-                <div class="detail">
-                    <h3>ゲームタイトル</h3>
-                    <p class="desc">ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文</p>
-                    <div class="bottom-info">
-                        <p class="info">何かの情報</p>
-                        <p class="ranking"><a href="#">ランキングを見る</a></p>
-                    </div><!-- bottom-info -->
-                </div><!-- detail -->
-            </div><!-- game -->
-            
-            <div class="game">
-                <p class="game-image"><img src="./image/gameImage.png" alt="ゲーム名"></p>
-                <div class="detail">
-                    <h3>ゲームタイトル</h3>
-                    <p class="desc">ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文</p>
-                    <div class="bottom-info">
-                        <p class="info">何かの情報</p>
-                        <p class="ranking"><a href="#">ランキングを見る</a></p>
-                    </div><!-- bottom-info -->
-                </div><!-- detail -->
-            </div><!-- game -->
+            <?php for ($i=0; $i<3 ; $i++): ?>
+                <div class="game">
+                    <p class="game-image"><img src="./image/gameImage.png" alt="ゲーム名"></p>
+                    <div class="detail">
+                        <h3>ゲームタイトル</h3>
+                        <p class="desc">ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文ゲーム紹介文</p>
+                        <div class="bottom-info">
+                            <p class="info">何かの情報</p>
+                            <p class="ranking"><a href="#">ランキングを見る</a></p>
+                        </div><!-- bottom-info -->
+                    </div><!-- detail -->
+                </div><!-- game -->
+            <?php endfor ?>
         </section><!-- game-list -->
     </main>
 </body>
