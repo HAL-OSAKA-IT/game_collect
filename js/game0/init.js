70c9ca0652ee280ef912978c9e47a09d386aa6ee
morita-32 <it11b.32moritashiyu@gmail.com>
2023-12-26 13:11:12 +0900

    ゲーム画面を作成
    タイトルと説明文の表示の実装は頼みました

---
+++ js/game0/init.js
@@ -0,0 +1,39 @@
+console.log('game0/init.js loaded!');
+// このファイルはゲームの処理を行うスクリプトを読み込むためのファイルです
+// 必要なファイルを適宜以下に追記していってください
+
+// 読み込むスクリプトのファイル名(適宜変更)
+// これらファイルは同じ数字のgameフォルダに格納すること
+// 上に書いたものから順に読み込まれる
+const scriptName = [
+	//"aaa.js",
+	//"bbb.js",
+	//"ccc.js"
+];
+
+// ゲームタイトルを以下に記入
+const gameTitle = "";
+// ゲームの説明文を以下に記入
+const gameDesc = "";
+
+
+// 以下変更の必要なし
+
+// 現在のgameフォルダのパスを求める
+const searchParams = new URLSearchParams(window.location.search);
+var gameID;
+if(searchParams.get("gameID") === null) {
+	gameID = "0"
+}else {
+	gameID = searchParams.get("gameID");
+}
+const gamePath = "./../js/game" + gameID + "/";
+// script要素を必要数生成
+const scripts = [];
+for(let i=0; i<scriptName.length; i++) {
+	scripts[i] = document.createElement('script');
+	scripts[i].src = gamePath + scriptName[i];
+	console.log(scripts[i].src);
+	document.body.appendChild(scripts[i]);
+}
+
